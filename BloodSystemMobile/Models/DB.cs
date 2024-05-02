756e85747addefc5e041b340f4aa2aaa748be5e5
Nethsarani <nethsaraniprasangika09@gmail.com>
2024-05-02 23:18:47 +0530

    Initialization

--- BloodSystemMobile/Models/DB.cs
+++ BloodSystemMobile/Models/DB.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Xml.Serialization;
 using BloodDonationManamentSystem.Models;
 using System.Security.RightsManagement;
+using Microsoft.Data.SqlClient;
 
 namespace BloodDonationManamentSystem
 {
