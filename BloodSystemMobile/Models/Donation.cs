3ce81fca3eaf1c5fa6dd492ed9459cd0275bc969
Nethsarani <129503280+Nethsarani@users.noreply.github.com>
2024-05-17 07:55:46 +0530

    update BloodSystemMobile/Models/Donation.cs

--- BloodSystemMobile/Models/Donation.cs
+++ BloodSystemMobile/Models/Donation.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace BloodDonationManamentSystem
 {
+  [Table("DonationTable")]
     public class Donation
     {
+      [PrimaryKey]
+      [AutoIncrement]
+      [Column("ID")]
         public int ID { get; set; }
+        
+              [Column("DonorID")]
         public Donor Donor { get; set; }
+        
+              [Column("Place")]
         public CollectionPoint collectionPoint { get; set; }
+        
+              [Column("Date")]
         public DateTime Date { get; set; }
+        
+                      [Column("Status")]
+        public string Status { get; set; }
     }
 }
