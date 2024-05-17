aa90ed5d83f337d09c64707ae2034d24042c362f
Nethsarani <129503280+Nethsarani@users.noreply.github.com>
2024-05-17 12:59:37 +0530

    update BloodSystemMobile/Models/Request.cs

--- BloodSystemMobile/Models/Request.cs
+++ BloodSystemMobile/Models/Request.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace BloodDonationManamentSystem.Models
 {
+  [Table("RequestTable")]
     public class Request
     {
+      [PrimaryKey]
+      [AutoIncrement]
+      [Column("Id")]
         public int Id { get; set; }
+        
+        [Column("HospitalID")]
+        public int HosId { get; set; }
+        
         public Hospital Hospital { get; set; }
+        
+        [Column("Date")]
         public DateTime Date { get; set; }
+        
+        [Column("BloodType")]
         public string BloodType { get; set; }
+        
+        [Column("Amount")]
         public decimal BloodAmount { get; set; }
     }
 }
