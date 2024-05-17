444ad8e06d3f15367c75e39b484ff96f43fbe44f
Nethsarani <129503280+Nethsarani@users.noreply.github.com>
2024-05-17 07:51:46 +0530

    update BloodSystemMobile/Models/Appointment.cs

--- BloodSystemMobile/Models/Appointment.cs
+++ BloodSystemMobile/Models/Appointment.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite
 
 namespace BloodDonationManamentSystem.Models
 {
+  [Table("AppointmentTable")]
     public class Appointment
     {
+      [PrimaryKey]
+      [AutoIncrement]
+      [Column("Id")]
         public int Id { get; set; }
+        
+        [Column("DonorID")]
         public Donor Donor { get; set; }
+        
+        [Column("CollectionPointID")]
         public CollectionPoint Place { get; set; }
+        
+        [Column("Date")]
         public DateTime Date { get; set; }
+        
+        [Column("Time")]
         public TimeSpan Time { get; set; }
+        
+        [Column("Description")]
         public string Description { get; set; }
+        
+        [Column("Status")]
+        public string Status { get; set; }
 
     }
 }
