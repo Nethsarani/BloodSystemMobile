047ebd0f719f979700508e390febba0b98e8dfd4
Nethsarani <129503280+Nethsarani@users.noreply.github.com>
2024-05-17 12:52:35 +0530

    update BloodSystemMobile/Models/Donor.cs

--- BloodSystemMobile/Models/Donor.cs
+++ BloodSystemMobile/Models/Donor.cs
@@ -8,21 +8,50 @@ using System.Threading.Tasks;
 
 namespace BloodDonationManamentSystem
 {
+  [Table("DonorTable")]
     public class Donor
     {
+      [PrimaryKey]
+      [AutoIncrement]
+      [Column("ID")]
         public int ID { get; set; }
+        
+        [Column("Name")]
         public string Name { get; set; }
+        
+        [Column("Gender")]
         public string Gender { get; set; }
+        
+        [Column("NIC")]
         public string NIC { get; set; }
+        
+        [Column("Location")]
         public Location Location { get; set; }
+        
+        [Column("DOB")]
         public DateTime DOB { get; set; }
+        
+        [Column("ContactNo")]
         public string ContactNo { get; set; }
+        
+        [Column("Email")]
         public string Email { get; set; }
+        
+        [Column("BloodType")]
         public string BloodType { get; set; }
+        
+        [Column("HealthCondition")]
         public HealthCondition health { get; set; }
+        
         public List<Donation> donations { get; set; }
+        
+        [Column("Username")]
         public string Username { get; set; }
+        
+        [Column("Password")]
         public string Password { get; set; }
+        
+        [Column("Status")]
         public string Status { get; set; }
     }
 }
